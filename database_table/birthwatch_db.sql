f9f5f13de0c2c3105a92cad39bb50fa3b9a21e50
ComradeHadi <comradehadi@gmail.com>
2014-01-15 17:01:04 -0800

    database renamed

---
+++ database_table/birthwatch_db.sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.9
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 16, 2014 at 01:57 AM
+-- Server version: 5.5.34
+-- PHP Version: 5.4.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `birthwatch`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `birth`
+--
+
+CREATE TABLE IF NOT EXISTS `birth` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `name_of_father` varchar(50) NOT NULL,
+  `name_of_mother` varchar(50) NOT NULL,
+  `place_of_brth` varchar(50) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `hospital` varchar(50) NOT NULL,
+  `district` varchar(50) NOT NULL,
+  `region` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `birth`
+--
+
+INSERT INTO `birth` (`id`, `first_name`, `last_name`, `name_of_father`, `name_of_mother`, `place_of_brth`, `date_of_birth`, `hospital`, `district`, `region`) VALUES
+(1, 'Hadi', 'Mukaila', 'Dinki', 'Boss', 'Accra', '2014-01-01', 'Korle-Bu', 'Districto', 'Gt. Accra');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `community`
+--
+
+CREATE TABLE IF NOT EXISTS `community` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `district` varchar(100) NOT NULL,
+  `region` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `community`
+--
+
+INSERT INTO `community` (`id`, `district`, `region`) VALUES
+(1, 'districto', 'Gt. Accra');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `death`
+--
+
+CREATE TABLE IF NOT EXISTS `death` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `date_of_death` date NOT NULL,
+  `place` varchar(50) NOT NULL,
+  `district` varchar(50) NOT NULL,
+  `region` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE IF NOT EXISTS `questions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `recipient_number` int(10) NOT NULL,
+  `question` varchar(160) NOT NULL,
+  `possible_response` varchar(160) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registrar`
+--
+
+CREATE TABLE IF NOT EXISTS `registrar` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `phone_number` int(10) NOT NULL,
+  `district` varchar(100) NOT NULL,
+  `region` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `responses`
+--
+
+CREATE TABLE IF NOT EXISTS `responses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sender` varchar(50) NOT NULL,
+  `questions` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
